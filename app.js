6ef20babb8bf88f3fde4848b4b2cf6299a959202
Abhijeet Patel <abhijeetpatelsagar@gmail.com>
2025-07-12 18:26:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 // app.js
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+window.register = function () {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert("Registration successful!");
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      alert("Registration failed: " + error.message);
+    });
+};
+
 import { auth } from './firebase-config.js';
 import {
   signInWithEmailAndPassword
